f89e3a360776368ce4714f9aa81cf5fb090163f3
wangcf9870 <1095422791@qq.com>
2024-11-12 15:05:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.wangcf.online"]: dockerHub,
+  ["quay.wangcf.online"]: "https://quay.io",
+  ["gcr.wangcf.online"]: "https://gcr.io",
+  ["k8s-gcr.wangcf.online"]: "https://k8s.gcr.io",
+  ["k8s.wangcf.online"]: "https://registry.k8s.io",
+  ["ghcr.wangcf.online"]: "https://ghcr.io",
+  ["cloudsmith.wangcf.online"]: "https://docker.cloudsmith.io",
+  ["ecr.wangcf.online"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.wangcf.online"]: dockerHub,
 };
 
 function routeByHosts(host) {
